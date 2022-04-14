4227afb6ce16fba31a52fd5e4926429b6f983ce0
Bob <andrey-gramovich@yandex.ru>
2022-04-14 23:39:39 +0300

    Initial commit

---
+++ src/com/company/User.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class User {
+    String name;
+    int age;
+    int weight;
+
+    public User(String name, int age, int weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+    }
+
+    public void eat(){
+        System.out.println("Я ем");
+    }
+    public void dayOfBirth(){
+        age++;
+    }
+
+}
